e98b683ae81852e5ea0471005fcd3b88738653c7
maliut <lqn619@163.com>
2017-12-03 21:48:01 +0800

    fix master destroy item

--- Assets/Scripts/GameItem/AddHPItem.cs
+++ Assets/Scripts/GameItem/AddHPItem.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using JetBrains.Annotations;
 
 public class AddHPItem : MonoBehaviour {
 
 	public int HpToAdd = 20;
 
+	private PhotonView photonView;
+
+	private void Start() {
+		photonView = GetComponent<PhotonView>();
+	}
+
 	private void OnCollisionEnter(Collision other) {
 		if (GameManager.gm.localPlayer.GetComponent<Collider>() != other.collider) return;
 		GameManager.gm.tankHealth.requestAddHP(HpToAdd);
+		photonView.RPC("DestroySelf", PhotonTargets.MasterClient);
+	}
+
+	[UsedImplicitly]
+	private void DestroySelf() {
+		if (!PhotonNetwork.isMasterClient) return;
 		PhotonNetwork.Destroy(gameObject);
 	}
 
